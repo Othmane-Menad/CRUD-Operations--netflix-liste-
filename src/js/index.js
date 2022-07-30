5a573d124e4b05474183bad0d8769ce87e7e44f6
othman31 <othman0031@gmail.com>
2022-07-31 00:27:28 +0100

    implemented  a delete functionalité

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,11 @@ const search = document
   .querySelector("#search")
   .addEventListener("keyup", searching);
 
+// Listener for delete button parent element
+const tbody = document
+  .querySelector("#tbody")
+  .addEventListener("click", removePost);
+
 // Get data from Api and populate in the table
 function loadData() {
   getData()
@@ -63,7 +68,6 @@ function searching(e) {
 
   e.preventDefault();
 }
-
 function searchTable(value, data) {
   const filteredData = [];
   data.map((element) => {
@@ -76,3 +80,63 @@ function searchTable(value, data) {
   });
   return filteredData;
 }
+
+// Delete Post
+function removePost(e) {
+  e.preventDefault();
+  // Checking if the parent has a child with delete class
+  if (e.target.classList.contains("delete")) {
+    // Get the id of the poste which we set as data-id in the ui showPosts function
+    // parent element is the a
+    const id = e.target.dataset.id;
+
+    deleteData(`http://localhost:3000/posts/${id}`)
+      .then((data) => {
+        loadData();
+      })
+      .catch((err) => console.log("Error while deleting", err));
+  }
+}
+
+// trying to add a key value to the object id with increment number
+// getData().then((data) => {
+//   const newList = [];
+//   let inc = 2;
+//   data.map((element) => {
+//     let {
+//       show_id,
+//       type,
+//       title,
+//       director,
+//       cast,
+//       country,
+//       date_added,
+//       release_year,
+//       rating,
+//       duration,
+//       listed_in,
+//       description,
+//     } = element;
+
+//     let newElement = {
+//       show_id,
+//       type,
+//       title,
+//       director,
+//       cast,
+//       country,
+//       date_added,
+//       release_year,
+//       rating,
+//       duration,
+//       listed_in,
+//       description,
+//       id: inc,
+//     };
+//     inc++;
+//     postData(newElement).then((data) => {
+//       console.log("success");
+//     });
+//     // newList.push(newElement);
+//   });
+// });
--- src/js/ui.js
+++ src/js/ui.js
@@ -2,18 +2,31 @@ export function populateTable(data) {
   const body = document.querySelector("#tbody");
   let query = "";
   data.map((element) => {
-    let { show_id, type, title, director, release_year } = element;
+    let { id, type, title, director, release_year } = element;
     query += `
     <tr>
-    <td>${show_id}</td>
+    <td>${id}</td>
     <td>${type}</td>
     <td>${title}</td>
     <td>${director}</td>
     <td>${release_year}</td>
+    <td><a href="#" class="delete" data-id="${id}"
+    >X</a></td>
+    <td><a href="#" class=" edit deep-purple-text" data-id="${id}">
+    E
+  </a></td>
+
     </tr>
     `;
   });
   body.innerHTML = query;
 }
 
-
+/* <div class="card-action">
+  <a href="#" class=" edit deep-purple-text" data-id="${show_id}">
+    <i class="fa fa-pencil"></i>
+  </a>
+  <a href="#" class="delete" data-id="${id}">
+    <i class="fa fa-remove"></i>
+  </a>
+</div>; */
